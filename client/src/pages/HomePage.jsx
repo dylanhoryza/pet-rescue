bee2f99f7d7b1f81338f1dac50d3a43d82631433
dylan horyza <horyza.dylan@gmail.com>
2024-04-10 16:23:16 -0700

    updated homepage image and navbar

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import '../styles/homepage.css';
+import logoImage from '../assets/regaldane-transformed.png';
 
 export default function HomePage() {
   return (
-    <div>Home</div>
-  )
+    <div className='home-container'>
+      <div className='home-about-section'>
+        <div className='title-container'>
+          <h3 className='home-title'>OUR MISSION</h3>
+          <p className='section-description'>
+            We are dedicated to the rescue, rehabilitation and rehoming of
+            abandoned, neglected and owner-surrendered Great Danes of all ages.
+            We also provide education to the public about the Great Dane breed
+            and about responsible pet ownership.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
