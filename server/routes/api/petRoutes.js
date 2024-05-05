f17d779b39de2148ec583d2d7898f9b94d2a6dd9
dylan horyza <horyza.dylan@gmail.com>
2024-05-05 16:13:05 -0700

    added multer, couldnt get photos to save to db

--- server/routes/api/petRoutes.js
+++ server/routes/api/petRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const multer = require('multer');
 const {
   createPet,
   updatePet,
@@ -6,8 +7,24 @@ const {
   getPets,
 } = require('../../controllers/petController');
 
+// Set up multer storage configuration
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '../uploads/'); // Specify the directory where uploaded files will be stored
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname); // Use the original file name for the uploaded file
+  }
+});
+
+// Initialize multer middleware with the configured storage
+const upload = multer({ storage: storage });
+
 // api/pets
-router.route('/').get(getPets).post(createPet);
+router.route('/').get(getPets)
+
+// router.post('/', upload.single('photo'), createPet);
+router.post('/', upload.array('photos', 5), createPet); 
 
 // api/pets/:petId
 router
